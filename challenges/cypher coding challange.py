5e33cbd8ad90c8ecd2df469c4becbb7d5de0e36a
samuelarm <1003@shenfield.essex.sch.uk>
2016-11-21 09:59:21 +0000

    Update cypher coding challange.py

--- challenges/cypher coding challange.py
+++ challenges/cypher coding challange.py
@@ -1,12 +1,13 @@
 #Samuel Armstrong
 #cypher coding challange
+import random
 
 user_input = input("> ")
 user_list = list(user_input)
 print (user_list)
 text=[]
 for i in user_list:
-    text.append(int(ord(i)))
+    text.append(ord(i))
 print(text)
 
 
@@ -17,11 +18,45 @@ for i in text:
 
     
 print (text)
-##map (int, text.split (','))
-##[int(s) for s in text.split(',')]
+
 
 user_list = ""
 for i in text:
-    user_list += chr( int(i, 10))
+    user_list += chr(i)
     
+print (user_list)
+print("\n")
+
+# with a random int as the + on now
+
+print ("now testing with a random interger")
+print ("\n")
+
+random_interger = random.randint(0,40)
+
+user_input = input("> ")
+user_list = list(user_input)
+print (user_list)
+text=[]
+for i in user_list:
+    text.append(ord(i))
+print(text)
+
+
+index = (0)
+for i in text:
+    text[0+index] = text[0+index] + random_interger
+    index += 1
+
+    
+print (text)
+
+
+user_list = ""
+for i in text:
+    user_list += chr(i)
+    
+print (user_list)
+print("\n")
+
 print (user_list)

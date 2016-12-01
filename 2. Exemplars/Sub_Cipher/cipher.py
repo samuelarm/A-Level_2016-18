0761716b69431aded0ee840aecf21c9ea898c1de
mrGorman <a.gorman@shenfield.essex.sch.uk>
2016-12-01 21:27:11 +0000

    Update

--- 2. Exemplars/Sub_Cipher/cipher.py
+++ 2. Exemplars/Sub_Cipher/cipher.py
@@ -2,23 +2,28 @@
     Name: Mr Gorman
     Date: 19/11/2016
     """
-
-CHARS = "abcdefghijklmnopqrstuvwxyz"
+# Global constants
+CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def process_text(text, option):
+    """ Function to encrypt or decrypt lowercase and uppercase alpha characters based on selected option.
+        """
     processed_text = ""
+    
     for char in text:
-        if char in CHARS or char in CHARS.upper():
+        if char in CHARS or char in CHARS.lower():
             if option == "e":
-                processed_text += chr(ord(char) + 2)
+                char = ord(char) + 2
+                if char > 90 and char < 97 or char > 122:
+                    char -= 26
             elif option == "d":
-                processed_text += chr(ord(char) - 2)
-        else:
-            processed_text += char
+                char = ord(char) - 2
+                if char < 97 and char > 90 or char < 65:
+                    char += 26
+            char = chr(char)
+        processed_text += char
+    
     return processed_text
-
-def get_text_from_file(filename):
-    pass
 
 def get_text_from_userinput():
     text = ""
@@ -33,7 +38,6 @@ def get_option():
     return option
 
 def main():
-
     option = get_option()
     text = get_text_from_userinput()
     text = process_text(text, option)

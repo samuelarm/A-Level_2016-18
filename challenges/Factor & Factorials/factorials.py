234cc79bac290f8616d6d968e2dd79c20d115016
samuelarm <1003@shenfield.essex.sch.uk>
2016-12-02 10:09:19 +0000

    Update factorials.py

--- challenges/Factor & Factorials/factorials.py
+++ challenges/Factor & Factorials/factorials.py
@@ -1,6 +1,6 @@
 #Samuel Armstrong
 #Coding Challange
-#could just usem range user_num but this is adapted from the factor finder code 
+#could just use range user_num but this is adapted from the factor finder code 
 user_num = int(input("> "))
 factor = []
 for i in range (1, user_num+1):

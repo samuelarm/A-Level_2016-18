4bc2f0b4e024d95cb26ba87c6c16147b94f47f5c
samuelarm <1003@shenfield.essex.sch.uk>
2017-03-31 10:50:25 +0100

    Update speed tracker.py
    updated with challenge one

--- challenges/speed tracker/speed tracker.py
+++ challenges/speed tracker/speed tracker.py
@@ -8,14 +8,46 @@ def speed_calculation (time_one, time_two, distance):
     average_speed = (distance/difference)
     return average_speed
 
-#testing 
+def speeding_message(miles_per_hour, speed_limit):
+    """ this is a function to work out if the car
+        is speeding the prints a message to notify
+        the user
+        """
+    if miles_per_hour > speed_limit:
+        print("This car was going over the speed limit")
+    elif miles_per_hour == speed_limit:
+        print("This car is travelling at the speed limit")
+    else:
+        print ("This car going under the speed limit")
+        
+
+def number_plate_validation (number_plate):
+    """ this is a function to work out if the
+        the number plate is corrrect doesn't
+        work
+        """
+    letter_one = test[:2]
+    number_one = test[2:4]
+    letter_two = test[5:]
+    if all ((letter_one.isalpha(), number_one.isdigit(), letter_two.isalpha())):
+        print("The number plate is valid")
+    else:
+        print ("The number plate is invalid")
+
+
+
+#testing
+#variable list
 distance = 1
-
 time_one = time.time()
-time_two = time_one + random.randint(20,40) 
-
+time_two = time_one + random.randint(50,100)
+speed_limit = 60
+test = ("xx22 xxx")
+# testing over
 
 miles_per_second = speed_calculation(time_one, time_two, distance)
 miles_per_hour = miles_per_second * 60 * 60
 miles_per_hour = round(miles_per_hour,2)
-print ("the car is traviling at",miles_per_hour,"mph")
+print ("The car is traviling at",miles_per_hour,"mph")
+speed_message = speeding_message(miles_per_hour, speed_limit)
+number_plate_test = number_plate_validation(test)
